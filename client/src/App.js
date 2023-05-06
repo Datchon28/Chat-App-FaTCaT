a3c9b037d4ff696293699c1907dcf63bcf7153d4
Nguyen Tien Dat <chienbinh5a2@gmail.com>
2023-05-06 11:58:43 +0700

    fix some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import ChatPage from './pages/ChatPage'
 import { io } from "socket.io-client"
 import SignUp from "./pages/Home/SignUp/SignUp"
 
-const ApiUrl = process.env.REACT_APP_SOCKET_API
+const ApiUrl = process.env.REACT_APP_SERVER_API
 const socket = io(ApiUrl, { })
 
 function App() {
